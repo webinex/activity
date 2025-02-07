e2e55d7524748f5e2237b1b6a386500b119df188
Webinex Dev <webinex.dev@gmail.com>
2025-02-07 10:18:30 +0300

    3.0.0-build1
    * Support of minimal API

---
+++ src/Webinex.Activity.AspNetCore/ControllerActionActivityMeta.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Webinex.Activity.AspNetCore;
+
+internal class ControllerActionActivityMeta : IActivityMeta
+{
+    private readonly HttpContext _httpContext;
+
+    private ControllerActionActivityMeta(
+        HttpContext httpContext)
+    {
+        _httpContext = httpContext;
+    }
+
+    public static bool TryGet(
+        HttpContext httpContext,
+        [NotNullWhen(true)] out ControllerActionActivityMeta? controllerActionActivityMeta)
+    {
+        controllerActionActivityMeta = null;
+        if (httpContext.ControllerActionDescriptor() == null) return false;
+
+        controllerActionActivityMeta = new ControllerActionActivityMeta(httpContext);
+        return true;
+
+    }
+
+    private ControllerActionDescriptor Descriptor => _httpContext.ControllerActionDescriptor()!;
+
+    public string Kind => Area == null
+        ? ActionKind
+        : $"{Area}:{ActionKind}";
+
+    private string ActionKind => ActivityAttr?.Kind ?? Descriptor.ActionName;
+
+    public bool IsActivity => HasActivityDecoration || IsImplicitActivity;
+
+    public bool IsImplicitActivity => _httpContext.IsImplicitMatch() && !ControllerNotActivity &&
+                                      !HasNotActivityAction && !HasActivityDecoration;
+
+    private string? Area
+    {
+        get
+        {
+            if (NoArea)
+                return null;
+
+            var controllerArea = Controller.GetCustomAttribute<ActivityAreaAttribute>()?.Area;
+            return !IsImplicitActivity ? controllerArea : controllerArea ?? Descriptor.ControllerName;
+        }
+    }
+
+    private bool NoArea => ActivityAttr?.NoArea == true;
+
+    private ActivityAttribute? ActivityAttr => Method.GetCustomAttribute<ActivityAttribute>();
+
+    private bool HasActivityDecoration => Method.GetCustomAttribute<ActivityAttribute>() != null;
+
+    private bool HasNotActivityAction => Method.GetCustomAttribute<NotActivityAttribute>() != null;
+
+    private bool ControllerNotActivity => Controller.GetCustomAttribute<NotActivityAttribute>() != null;
+
+    private TypeInfo Controller => Descriptor.ControllerTypeInfo;
+
+    private MethodInfo Method => Descriptor.MethodInfo;
+}
---
+++ src/Webinex.Activity.AspNetCore/HttpContextExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Webinex.Activity.AspNetCore;
+
+internal static class HttpContextExtensions
+{
+    public static IEndpointFeature? EndpointFeature(this HttpContext httpContext)
+    {
+        return httpContext.Features.Get<IEndpointFeature>();
+    }
+
+    public static ControllerActionDescriptor? ControllerActionDescriptor(this HttpContext httpContext)
+    {
+        return httpContext.EndpointFeature()?.Endpoint?.Metadata.GetMetadata<ControllerActionDescriptor>();
+    }
+
+    public static bool IsImplicitMatch(this HttpContext httpContext)
+    {
+        var settings = httpContext.RequestServices.GetRequiredService<IActivityAspNetCoreSettings>();
+        return settings.Implicit && (settings.ImplicitPredicate == null || settings.ImplicitPredicate(httpContext));
+    }
+
+    public static EndpointMetadataCollection? EndpointMetadata(this HttpContext httpContext)
+    {
+        return httpContext.EndpointFeature()?.Endpoint?.Metadata;
+    }
+
+    public static ActivityEndpointMetadata? ActivityEndpointMetadata(this HttpContext httpContext)
+    {
+        return httpContext.EndpointMetadata()?.GetMetadata<ActivityEndpointMetadata>();
+    }
+}
